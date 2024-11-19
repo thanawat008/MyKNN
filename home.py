580f599b047fc1c00a4ed287b704b15ed528091f
thanawat008 <Tnw10062546@gmail.com>
2024-11-19 14:54:49 +0700

    update

--- home.py
+++ home.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 st.title("Hello")
 st.header("Hello")
-st.subheader("Hello")
+st.subheader("Profile")
 
 st.image('./img/man.jpg')
 st.subheader("Thanawat Phongpae")
 
 dt=pd.read_csv('./data/iris-3.csv')
-st.header()
+st.header("ข้อมูลดอกไม้")
+st.write(dt.head(10))
