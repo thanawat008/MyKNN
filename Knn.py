74696a9781467e011f8c0b8bc66113c6b5509d23
thanawat008 <Tnw10062546@gmail.com>
2024-11-19 16:11:01 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -3,8 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
+
 
 st.header('kairung')
 #st.image("./pic/kairung.jpg")
