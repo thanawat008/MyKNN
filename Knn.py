55ffd8981f75022f03fb5fbc598426600ec9a3f8
thanawat008 <Tnw10062546@gmail.com>
2024-11-19 15:51:46 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -24,14 +24,14 @@ st.header('kairung')
 #st.image("./pic/kairung.jpg")
 col1, col2, col3 = st.columns(3)
 with col1:
-st.header("Versicolor")
-st.image("./img/iris1.jpg")
+ st.header("Versicolor")
+ st.image("./img/iris1.jpg")
 with col2:
-st.header("Verginiga")
-st.image("./img/iris2.jpg")
+ st.header("Verginiga")
+ st.image("./img/iris2.jpg")
 with col3:
-st.header("Setosa")
-st.image("./img/iris3.jpg")
+ st.header("Setosa")
+ st.image("./img/iris3.jpg")
 html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
@@ -80,5 +80,5 @@ elif out[0] == 'Versicolor':
  st.image("./img/iris2.jpg")
 else:
  st.image("./img/iris3.jpg")
-else:
- st.write("ไม่ทำนาย")
+ else:
+st.write("ไม่ทำนาย")
