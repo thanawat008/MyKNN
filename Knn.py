cd96abbd4e040a55a3c634f62e5b6a6b2113d40b
thanawat008 <Tnw10062546@gmail.com>
2024-11-19 15:56:56 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -75,10 +75,10 @@ x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
 st.write(Knn_model.predict(x_input))
 out=Knn_model.predict(x_input)
 if out[0] == 'Setosa':
- st.image("./img/iris1.jpg")
+    st.image("./img/iris1.jpg")
 elif out[0] == 'Versicolor':
- st.image("./img/iris2.jpg")
+    st.image("./img/iris2.jpg")
 else:
- st.image("./img/iris3.jpg")
- else:
-st.write("ไม่ทำนาย")
+    st.image("./img/iris3.jpg")
+else:
+    st.write("ไม่ทำนาย")
